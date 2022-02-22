18f6dd7a680cf2c2d689cc7a4bd03660f370d8dd
Eric Roos <ericroos@hey.com>
2022-02-22 12:32:17 -0600

    fix specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
-  def after_sign_in_path_for(user)
-    root_path
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   resources :projects do
     resources :requirements, shallow: true
   end
-  devise_for :users
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
