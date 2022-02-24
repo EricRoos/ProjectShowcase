069703f3d820ceb512573bc7f72c7a75b923b022
Eric Roos <ericroos@hey.com>
2022-02-24 11:34:51 -0600

    more i18n

--- app/helpers/landing_page_helper.rb
+++ app/helpers/landing_page_helper.rb
@@ -1,2 +1,10 @@
 module LandingPageHelper
+  def render_feature(feature_name, ini_i18n_scope='')
+    i18n_scope = "#{ini_i18n_scope}.features.#{feature_name}"
+    render partial: 'feature', locals: {
+      icon_name: t('icon_name', scope: i18n_scope),
+      name: t('name', scope: i18n_scope),
+      description: t('description', scope: i18n_scope)
+    }
+  end
 end
